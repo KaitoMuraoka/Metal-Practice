19f592d84d4c8a07d46b0a119efd81f1ce690d0e
KaitoMuraoka <kaitoboshi@icloud.com>
2025-10-26 12:52:33 +0900

    Initial Commit

---
+++ MetalSample/MetalSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalSampleApp.swift
+//  MetalSample
+//
+//  Created by KaitoMuraoka on 2025/10/26.
+//
+
+import SwiftUI
+
+@main
+struct MetalSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MetalSampleTests/MetalSampleTests.swift
@@ -0,0 +1,16 @@
+//
+//  MetalSampleTests.swift
+//  MetalSampleTests
+//
+//  Created by KaitoMuraoka on 2025/10/26.
+//
+
+import Testing
+
+struct MetalSampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
