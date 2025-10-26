44e91d253209543becb83ee7d679ad1e9506d860
KaitoMuraoka <kaitoboshi@icloud.com>
2025-10-26 13:03:50 +0900

    画像を表示用ファイルを作成

---
+++ MetalSample/Colors.swift
@@ -0,0 +1,20 @@
+//
+//  colors.swift
+//  MetalSample
+//
+//  Created by KaitoMuraoka on 2025/10/26.
+//
+
+import SwiftUI
+
+struct Colors: View {
+    var body: some View {
+        Image(.tonfly)
+            .resizable()
+            .scaledToFit()
+    }
+}
+
+#Preview {
+    Colors()
+}
